0820caf2b3bd1869420d2884da72cd916d31a8ac
Rafaela <rafaelaffigueiredo1@gmail.com>
2024-05-27 19:15:03 -0300

    js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const songList = [
     }
 ];
 
-const artistName = document.querySelector('.artist.name')
+const artistName = document.querySelector('.artist-name')
 const musicName = document.querySelector('.song-name');
 const fillBar = document.querySelector('.fill-bar');
 const time = document.querySelector('.time')
@@ -41,4 +41,66 @@ document.addEventListener('DOMContentLoaded', () =>{
     nextBtn.addEventListener('click', nextSong);
     playBtn.addEventListener('click', togglePlayPause);
     prog.addEventListener('click', seek);
-})
+});
+
+function loadSong(index){
+    const { name, artist, src, cover: thumb } = songList
+    [index];
+    artistName.innerText = artist;
+    musicName.innerText = name;
+    song.src = src;
+    cover.style.backgroundImage = `url(${thumb})`;
+}
+
+function updateProgress(){
+    if(song.duration){
+        const pos = (song.currentTime / song.duration) * 100;
+        fillBar.style.width = `${pos}%`;
+
+        const duration = formatTime(song.duration);
+        const currentTime = formatTime(song.currentTime);
+        time.innerText  = `${currentTime} - ${duration}`;
+    }
+}
+
+function formatTime(seconds){
+    const minutes = Math.floor(seconds / 60);
+    const secs = Math.floor(seconds % 60);
+    return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
+}
+
+function togglePlayPause(){
+    if(playing){
+        song.pause();
+    }else {
+        song.play();
+    }
+    playing = !playing;
+    playBtn.classList.toggle('fa-pause', playing);
+    playBtn.classList.toggle('fa-play', !playing);
+    cover.classList.toggle('active', playing)
+}
+
+function nextSong(){
+    currentSong = (currentSong + 1) % songList.length;
+    playMusic();
+}
+
+function prevSong(){
+    currentSong = (currentSong - 1 + songList.length) % songList.length;
+    playMusic();
+}
+
+function playMusic(){
+    loadSong(currentSong);
+    song.play();
+    playing = true;
+    playBtn.classList.add('fa-pause');
+    playBtn.classList.remove('fa-play');
+    cover.classList.add('active');
+}
+
+function seek(e){
+    const pos = (e.offsetX / prog.clientWidth) * song.duration;
+    song.currentTime = pos;
+}
